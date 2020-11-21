4e954383a8a3db12cdecd3122ea604148316d70e
Vishesh Garg <visheshgarg5226@gmail.com>
2020-11-22 01:33:42 +0530

    initial commit

---
+++ single_compresscode.py
@@ -0,0 +1,23 @@
+#-----------------------------------single-image compression code-----------------------------
+
+
+import PIL 					#-----------------------------------Pillow library
+from PIL import Image
+
+mywidth = 5136				#-------------------------------------image width
+
+
+#--------------------------compression code--------------------------------------------------- 
+
+
+img = Image.open(r'<enter the image path>') #------------original image path
+
+wpercent = (mywidth/float(img.size[0]))
+hsize = int((float(img.size[1]) * float(wpercent)))
+img = img.resize((mywidth,hsize), PIL.Image.ANTIALIAS)
+
+img.save(r'<enter the path were you want to save the image>')  #-------------------------compressed image path
+
+
+
+'''-------------------------------------code end-------------------------------------------'''
